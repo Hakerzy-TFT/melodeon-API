70251183dcadc6213d2bb4346d338d2a6cdbbbeb
Piotr <p.mroz.zywiol@gmail.com>
2021-02-06 22:48:51 +0100

    Initial commit

---
+++ Controllers/ServiceStatus.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MelodeonApi.Controllers
+{
+    public class ServiceStatus : ControllerBase
+    {
+        
+    }
+}
---
+++ Models/Token.cs
@@ -0,0 +1,9 @@
+
+
+namespace MelodeonApi.Models
+{
+    class Token
+    {
+        
+    }
+}
