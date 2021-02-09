574aac5df9328b81ce8f497f021f7569e835c42e
Kacper <kacper.plusaa@gmail.com>
2021-02-10 00:00:16 +0100

    added token model with configuration id

--- Models/Token.cs
+++ Models/Token.cs
@@ -5,16 +5,22 @@ using MongoDB.Bson.Serialization.Attributes;
 using System;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace MelodeonApi.Models
 {
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
     public class Token
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string _id { set; get; }
-        public string Name { set; get; }
-        public string AuthString { set; get; }
-        public string Created { set; get; }
+        public string _id { get; set; }
+        public string TokenAuth { get; set; }
+        public string SpotifyAuth { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Configuration { get; set; }
     }
+
 }
